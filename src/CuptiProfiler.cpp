552a1f6c27ce26a785fd09b37a7e3480b4afc440
anshumang <anshumang@gatech.edu>
2015-06-21 15:56:35 -0400

    Window and Predictor stubs and uncommitted Xrush and Interposer files

--- src/CuptiProfiler.cpp
+++ src/CuptiProfiler.cpp
@@ -1,5 +1,5 @@
 /*
- * This file is part of cupti_profiler
+ * This file is part of gpu_use_idle_predictor
  * 
  * cupti_profiler is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
---
+++ src/Xrush.cpp
@@ -0,0 +1,58 @@
+/*
+ * This file is part of gpu_use_idle_predictor
+ * 
+ * gpu_use_idle_predictor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * gpu_use_idle_predictor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gpu_use_idle_predictor. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author: Anshuman Goswami <anshumang@gatech.edu>
+ */
+
+#include "CuptiProfiler.h"
+#include "Interposer.h"
+#include "Predictor.h"
+#include "Window.h"
+
+extern CuptiProfiler *p_profiler;
+extern Interposer *p_interposer;
+extern Predictor *p_predictor;
+extern Window *p_window;
+
+extern "C"
+void XrushCreate()
+{
+   p_profiler = new CuptiProfiler();
+   p_interposer = new Interposer();
+   p_predictor = new Predictor();
+   p_window = new Window();
+}
+
+extern "C"
+void XrushDestroy()
+{
+   delete p_profiler;
+   delete p_interposer;
+   delete p_predictor;
+   delete p_window;
+}
+
+extern "C"
+void XrushProfile()
+{
+   p_profiler->read();
+}
+
+extern "C"
+void XrushLaunch(unsigned long gridX, unsigned long gridY, unsigned long gridZ, unsigned long blockX, unsigned long blockY, unsigned long blockZ)
+{
+   p_interposer->launch(gridX, gridY, gridZ, blockX, blockY, blockZ);
+}
